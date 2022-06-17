d5f78ea8229fec344b32f6d94e07b5ff8337eaba
Metone1 <Evgeny.Chernov.One@gmail.com>
2022-06-17 16:30:38 +0300

    add_one more slider(problems with click on bullet)

--- script.js
+++ script.js
@@ -19,11 +19,31 @@ const swiper = new Swiper('.surf__swiper', {
    centeredSlides: 'true',
 });
 
+const swiper_travel = new Swiper(
+   '.travel__swiper', {
+
+   direction: 'horizontal',
+   loop: true,
+
+   pagination: {
+      el: '.surf__swiper-pagination',
+      // clickable: 'true',
+   },
+   navigation: {
+      nextEl: '.swiper__button-right',
+      prevEl: '.swiper__button-left',
+   },
+   grabCursor: 'true',
+   slideToClickedSlide: 'true',
+   slidesPerView: '1',
+   spaceBetween: 0,
+   centeredSlides: 'true',
+}
+)
+
 const addClassName = (() => {
    let bullets = document.querySelectorAll('.swiper-pagination-bullet');
    for (let i = 0; i < bullets.length; i++) {
-      console.log(i);
-      console.log(bullets[i]);
       bullets[i].classList.add(`surf__background-oval_${[i]}`);
    }
 })();
